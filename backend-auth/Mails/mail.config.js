39ed83822e1b345cabaa6d0454dd509592308d78
K.B.Abhishek <88060711+abishe8@users.noreply.github.com>
2025-07-28 08:58:22 +0530

    Update mail.config.js

--- backend-auth/Mails/mail.config.js
+++ backend-auth/Mails/mail.config.js
@@ -2,10 +2,10 @@ import nodemailer from "nodemailer"
 import { google } from "googleapis"
 
 
-const CLIENT_ID = '888811781592-m9cauprm45jmk9bscseq8j0p9ig1qurn.apps.googleusercontent.com'
-const CLIENT_SECRET = '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'
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
-const REFRESH_TOKEN = '1//04ObavX7OLYY5CgYIARAAGAQSNwF-L9Ir5fEs6b18koe5eDS0tSdeJNMuP7qwLZlI_rCVHOMXJFCBW82H4y17Aqtfp4OoCdjpRkM'
+const CLIENT_ID = ''
+const CLIENT_SECRET = ''
+const REDIRECT_URI = ''
+const REFRESH_TOKEN = ''
 
 const oAuth2Client = new google.auth.OAuth2( CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
